7e00fceda9b883165d39f06f526bd0e62a661bf3
Emin <krates01@gmail.com>
2023-08-08 19:37:37 +0300

    Ready

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyLib;
+
+// Урок 9. Рекурсия
+
+// Задача 66: Задайте значения M и N. Напишите рекурсивный метод, который найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+int Task66(int M, int N, int sum = 0)
+{
+    if (M > N) return sum;
+    sum = sum + M;
+    return Task66(M + 1, N, sum);
+}
+
+Console.Clear();
+
+int M = MyLibClass.Input("Введите значение M: ");
+int N = MyLibClass.Input("Введите значение N: ");
+int sum = Task66(M, N);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N равна: {sum}");
+
---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿using MyLib;
+
+// Урок 9. Рекурсия
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Task68(int m, int n)
+{
+
+    if (m == 0) return n + 1;
+    else if (n == 0) return Task68(m - 1, 1);
+    else return Task68(m - 1, Task68(m, n - 1));
+}
+
+Console.Clear();
+
+int m = MyLibClass.Input("Введите значение m: ");
+int n = MyLibClass.Input("Введите значение n: ");
+
+Console.WriteLine(Task68(m, n));
+
+
+
+
